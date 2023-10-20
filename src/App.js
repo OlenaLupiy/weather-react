3d9ed2eceaaaa0f0d3d066751c65a2706c924c7a
OlenaLupiy <ollupiy@gmail.com>
2023-10-20 11:51:23 +0100

    cleaned app.css, app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+
 import Weather from "./Weather";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Weather App</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-        <Weather />
-      </header>
+      <h1>Weather App</h1>
+
+      <Weather />
     </div>
   );
 }
